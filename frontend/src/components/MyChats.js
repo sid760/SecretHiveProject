ded2ca31c14dea079c4fd84814fe5e25e80bf69d
Siddharth Mishra <mishrasiddharth760@gmail.com>
2023-12-09 22:48:34 +0530

    the profile modal icon changed from view icon to info icon

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -47,7 +47,7 @@ const MyChats = ({ fetchAgain }) => {
 
   return (
     <Box
-      d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
       flexDir="column"
       alignItems="center"
       p={3}
--- frontend/src/components/miscellaneous/ProfileModal.js
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -1,4 +1,4 @@
-import { ViewIcon } from "@chakra-ui/icons";
+import { InfoIcon } from "@chakra-ui/icons";
 import {
   Modal,
   ModalOverlay,
@@ -22,7 +22,11 @@ const ProfileModal = ({ user, children }) => {
       {children ? (
         <span onClick={onOpen}>{children}</span>
       ) : (
-        <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+        <IconButton
+          display={{ base: "flex" }}
+          icon={<InfoIcon />}
+          onClick={onOpen}
+        />
       )}
       <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
