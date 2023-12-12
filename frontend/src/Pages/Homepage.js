395741e46379856f58d07365c10569b219f137cf
Ath-arv <83336617+Ath-arv@users.noreply.github.com>
2023-12-13 00:05:38 +0530

    Homepage animation added

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -12,6 +12,8 @@ import { useEffect } from "react";
 import { useHistory } from "react-router";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
+import Lottie from "react-lottie";
+import animationData from "../Animation - 1702404269691.json"; // Import your Lottie animation JSON file
 
 function Homepage() {
   const history = useHistory();
@@ -22,46 +24,62 @@ function Homepage() {
     if (user) history.push("/chats");
   }, [history]);
 
+  // Lottie options for your animation
+  const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <Container maxW="xl" centerContent>
-      <Box
-        display="flex"
-        justifyContent="center"
-        p={3}
-        bg="white"
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text fontSize="4xl" fontFamily="Work sans">
-          SecretHive
-        </Text>
-      </Box>
-      <Box
-        bg="white"
-        w="100%"
-        p={4}
-        borderRadius="lg"
-        borderWidth="1px"
-        mb="25%"
-      >
-        <Tabs isFitted variant="soft-rounded">
-          <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Login />
-            </TabPanel>
-            <TabPanel>
-              <Signup />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
+    <Box position="relative" height="100vh">
+      {/* Lottie Animation */}
+      <Lottie options={lottieOptions} height="100%" width="100%" />
+
+      {/* Content */}
+      <Container maxW="xl" centerContent position="absolute" top="0" left="0" right="0" bottom="0">
+        <Box
+          d="flex"
+          justifyContent="center"
+          p={3}
+          bg="white"
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Text fontSize="4xl" fontFamily="Work sans" fontWeight="bold">
+            SecretHive
+          </Text>
+        </Box>
+        <Box
+          bg="white"
+          w="100%"
+          p={4}
+          borderRadius="lg"
+          borderWidth="1px"
+          mb="25%"
+        >
+          <Tabs isFitted variant="soft-rounded">
+            <TabList mb="1em">
+              <Tab>Login</Tab>
+              <Tab>Sign Up</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
